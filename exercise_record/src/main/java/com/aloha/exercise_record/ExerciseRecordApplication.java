18fd5f2c505486c40362aec4b09e0da0cac2b3f5
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-21 19:09:24 +0900

    운동기록 프로젝트

---
+++ exercise_record/src/main/java/com/aloha/exercise_record/ExerciseRecordApplication.java
@@ -0,0 +1,13 @@
+package com.aloha.exercise_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseRecordApplication.class, args);
+	}
+
+}
